65f68922ad612ffc524c2c6b23f6f196d51e6cbb
yuusaku125521 <one25Yellow@icloud.com>
2020-12-22 22:05:18 +0900

    0u9h

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,32 +1,30 @@
 import React from "react";
 import Header from "../templates/Header/Header";
 import Slider from "../templates/Slider/Slider"
-// import Insta from "../templates/instagram/instagram"
+// import Insta from "..//templates/instagram/instagram"
 import Footer from "../templates/Footer/Footer"
 
 const Home = () => {
 
-  const inner ={
+  const inner = {
     padingTop: "50px",
     width: "80%",
-  margin: "0 auto",
-  maxWidth: "500px",
-   }
-  const bodyInner ={
+    margin: "0 auto",
+    maxWidth: "500px",
+  }
+  const bodyInner = {
     background: "#fff8dc"
   }
- 
+
   return (
-    <div style = {bodyInner}>
+    <div style={bodyInner}>
       <Header />
       <div style={inner}>
-        <Slider  />
-        {/* <Layout>
-        <Insta />
-        </Layout> */}
-       </div>
-      
-       <Footer />
+        <Slider />
+         
+      </div> 
+ {/* <Insta /> */}
+      <Footer />
     </div>
 
   )
