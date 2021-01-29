7f446c6e7e2868f41214baf4b6d0e50389e24045
yuusaku125521 <one25Yellow@icloud.com>
2021-01-29 23:58:19 +0900

    main

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../templates/Header/Header";
 import Footer from "../templates/Footer/Footer";
+import TextField from '@material-ui/core/TextField';
+import { ContentWarp,TelWarp, MaliWarp, NameWarp, ContactBody} from "./ContactStyle";
 
 
 
@@ -12,9 +14,25 @@ const Contact = () => {
   return (
     <>
       <Header />
+      <ContactBody>
+        <NameWarp><TextField id="outlined-basic" label="name" variant="outlined" /></NameWarp>
+        < MaliWarp><TextField id="outlined-basic" label="e-mail" variant="outlined" /></ MaliWarp>
+        <TelWarp> <TextField id="outlined-basic" label="tel" variant="outlined" /></TelWarp>
+        <ContentWarp><TextField
+        id="outlined-multiline-static"
+        label="Contents"
+        multiline
+        rows={4}
+        defaultValue="お問い合わせ内容"
+        variant="outlined"
+      />
+        </ContentWarp>
+      </ContactBody>
 
 
-      <Footer />
+  <Footer />
+     
+    
 
     </>
   )
---
+++ src/components/pages/ContactStyle.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+
+
+
+export const ContactBody = styled.div`
+  display:flex;
+  align-items: center;
+  justify-content: center;
+ flex-direction: column;
+`
+
+export const NameWarp = styled.div`
+margin:20px;
+`
+export const MaliWarp = styled.div`
+margin:20px;
+`
+export const TelWarp = styled.div`
+margin:20px;
+`
+export const ContentWarp = styled.div`
+margin:20px;
+`
+
